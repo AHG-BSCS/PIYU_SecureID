6ab4d01e0da2068c4ebce55e32409522c6992d57
Valdezin <ops@vldz.cf>
2023-12-09 13:09:11 +0800

    Add project files.

---
+++ PIYU_SecureID/Form1.cs
@@ -0,0 +1,35 @@
+using OtpNet;
+
+namespace PIYU_SecureID
+{
+    public partial class Form1 : Form
+    {
+        private const string SecretKey = "F7XO6UWCIHG3XAZ4JSBTUYLZVJECUBH7";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string userCode = textBox1.Text;
+            if (ValidateCode(SecretKey, userCode))
+            {
+                MessageBox.Show("Authentication Success!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Hide();
+                Form dashboard = new dashboard();
+                dashboard.Show();
+            }
+            else
+            {
+                MessageBox.Show("Authentication failed! Please make sure that you are authorized to access this!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool ValidateCode(string secretKey, string userCode)
+        {
+            var totp = new Totp(Base32Encoding.ToBytes(secretKey));
+            return totp.VerifyTotp(userCode, out _, new VerificationWindow(2, 2)); // Validate within ±2 time steps.
+        }
+    }
+}
---
+++ PIYU_SecureID/dashboard.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace PIYU_SecureID
+{
+    partial class dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "plan of operations: (logo)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "create id";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(18, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "check id database";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(18, 202);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "verify id";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 409);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "current date and time";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(197, 35);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(591, 394);
+            this.panel1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 337);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "display panel ->";
+            // 
+            // dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "dashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "dashboard";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label2;
+        private Panel panel1;
+        private Label label3;
+    }
+}
