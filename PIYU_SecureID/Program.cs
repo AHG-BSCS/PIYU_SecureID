6482568be58e5767b374e05c268d6c151f6766e4
blueorion05 <jhudielatienza2k20@gmail.com>
2023-12-11 20:40:26 +0800

    try(2)

--- PIYU_SecureID/Program.cs
+++ PIYU_SecureID/Program.cs
@@ -10,12 +10,8 @@ namespace PIYU_SecureID
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            if (Environment.OSVersion.Version.Major >= 6) SetProcessDPIAware();
             ApplicationConfiguration.Initialize();
             Application.Run(new FormLoad());
         }
-
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        private static extern bool SetProcessDPIAware();
     }
 }
